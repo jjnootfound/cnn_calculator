92f278b1b98383945fc3c4f87861d98180144a81
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-04-09 22:00:07 -0600

    fix: code format and function arrangement

--- graphical_calculator.py
+++ graphical_calculator.py
@@ -12,18 +12,6 @@ class GraphicalCalculator(tk.Tk):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
-        # Action buttons
-        clear_button = Button(main_frame, text="Borrar", command=self.clear_canvas)
-        clear_button.grid(row=1, column=0, sticky=(W, E))
-
-        #Button for calculating, actions undefined because of the lack of other processes such as opencv and pytorch
-        calculate_button = Button(main_frame, text="Calcular", command=lambda: result_label.config(text="Tu resultado es: [en proceso]"))
-        calculate_button.grid(row=1, column=1, sticky=(W, E))
-
-        # results
-        self.result_label = Label(main_frame, text="Tu resultado es: [en proceso]", background="white")
-        self.result_label.grid(row=2, column=0, columnspan=2, sticky=(W, E))
-
 def savePosn(event):
     global lastx, lasty
     lastx, lasty = event.x, event.y
@@ -34,7 +22,6 @@ def addLine(event):
 
 def clear_canvas():
     canvas.delete("all")
-    result_label.config(text="Tu resultado es: [en proceso]")  # resets label when restarting
 
 # Window config
 root = Tk()
@@ -49,20 +36,30 @@ main_frame.columnconfigure(0, weight=1)
 main_frame.rowconfigure(0, weight=1)
 
 # Canvas config
-canvas = Canvas(main_frame, background='white', width=400, height=300)
+canvas = Canvas(main_frame, background="white", width=400, height=300)
 canvas.grid(row=0, column=0, columnspan=2, sticky=(N, W, E, S))
 canvas.bind("<Button-1>", savePosn)
 canvas.bind("<B1-Motion>", addLine)
+
+def update_result_label(text):
+    result_label.config(text=text)
 
 # Action buttons
 clear_button = Button(main_frame, text="Borrar", command=clear_canvas)
 clear_button.grid(row=1, column=0, sticky=(W, E))
 
-calculate_button = Button(main_frame, text="Calcular", command=lambda: result_label.config(text="Tu resultado es: [en proceso]"))
+ # Button for calculating, actions undefined because of the lack of other processes such as opencv and pytorch
+calculate_button = Button(
+    main_frame,
+    text="Calcular",
+    command=lambda: update_result_label("Tu resultado es: [en proceso]"),
+)
 calculate_button.grid(row=1, column=1, sticky=(W, E))
 
-#Label for printing the results of the calculation
-result_label = Label(main_frame, text="Tu resultado es: [en proceso]", background="white")
+# Label for printing the results of the calculation
+result_label = Label(
+    main_frame, text="Tu resultado es: [en proceso]", background="white"
+)
 result_label.grid(row=2, column=0, columnspan=2, sticky=(W, E))
 
 root.mainloop()
